0f7ece13b89aa9d5715d0137b3e93c5003b784f6
Wangxuan0528 <38967382+Wangxuan0528@users.noreply.github.com>
2018-06-28 19:36:22 +0800

    Unity异步加载场景显示进度
    Unity异步加载场景显示进度。两种实现方式。

---
+++ LoadScene_Async_While.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoadScene_Async_While : MonoBehaviour {
+    public Text text;
+    public Image image;
+    AsyncOperation ao;
+
+	void Start () {
+        image.fillAmount = 0;
+        StartCoroutine(Loading());
+	}
+
+    IEnumerator Loading()
+    {
+        int currentValue = 0;
+        int wantValue = 0;
+
+        ao = SceneManager.LoadSceneAsync("Demo2_towers");
+        ao.allowSceneActivation = false;
+
+        while (ao.progress < 0.9f)
+        {
+            wantValue =(int) ao.progress * 100;
+            while (currentValue < wantValue)
+            {
+                ++currentValue;
+                image.fillAmount = currentValue / 100f;
+                text.text = currentValue + "%";
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+
+        wantValue = 100;
+
+        while (currentValue < wantValue)
+        {
+            ++currentValue;
+            image.fillAmount = currentValue / 100f;
+            text.text = currentValue + "%";
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        text.text = (image.fillAmount * 100).ToString("0") + "%";
+        Debug.Log("加载完成");
+        ao.allowSceneActivation = true;
+    }
+}
